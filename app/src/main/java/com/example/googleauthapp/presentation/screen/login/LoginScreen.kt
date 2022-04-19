e9071b35752c2fdd2dea7e287966b3ac2046f135
Bruno <bruno.g.p897@gmail.com>
2022-04-19 16:07:38 -0300

    Create LoginViewModel

--- app/src/main/java/com/example/googleauthapp/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/googleauthapp/presentation/screen/login/LoginScreen.kt
@@ -2,22 +2,31 @@ package com.example.googleauthapp.presentation.screen.login
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.example.googleauthapp.domain.model.MessageBarState
+import com.example.googleauthapp.navigation.Screen
 
 @Composable
 fun LoginScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    loginViewModel: LoginViewModel = hiltViewModel()
 ) {
+
+    val signedInState by loginViewModel.signedInState
+    val messageBarState by loginViewModel.messageBarState
+
     Scaffold(
         topBar = {
             LoginTopBar()
         },
         content = {
             LoginContent(
-                signedInState = false,
-                messageBarState = MessageBarState(),
-                onButtonClicked = {}
+                signedInState = signedInState,
+                messageBarState = messageBarState,
+                onButtonClicked = {
+                    loginViewModel.saveSignedInState(signedIn = true)
+                }
             )
         }
     )
