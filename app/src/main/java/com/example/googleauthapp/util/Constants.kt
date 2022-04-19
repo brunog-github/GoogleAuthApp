78b0d03b39a224fc7409c350d202e14d4475d6f7
Bruno <bruno.g.p897@gmail.com>
2022-04-19 16:28:39 -0300

    One Tap - Sign in/Sign up

--- app/src/main/java/com/example/googleauthapp/util/Constants.kt
+++ app/src/main/java/com/example/googleauthapp/util/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
 
     const val PREFERENCES_NAME = "app_preferences"
     const val PREFERENCES_SIGNED_IN_KEY = "signed_in_key"
+
+    const val CLIENT_ID = "371680380991-f23137mr4kkud155r416h2k6t3hpmhpj.apps.googleusercontent.com"
 }
